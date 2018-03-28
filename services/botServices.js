2f2956c39b2d1e11bfc713e2a36ad76bde8ee00d
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2018-03-28 13:40:51 +0530

    The agent has been changed

--- services/botServices.js
+++ services/botServices.js
@@ -1,6 +1,6 @@
 
 var apiai = require('apiai');
-var apiApp = apiai("502a8828a2ba4179abe9b9f55ec85d6d");
+var apiApp = apiai("306d73e591d14a7e944ea92feaf65945");//502a8828a2ba4179abe9b9f55ec85d6d
 var getSessionId = function (session) {    
     if(!session){
         return "0d6036e2370c4cb59ba989763c69d1aa";
